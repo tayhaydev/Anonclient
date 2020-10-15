847a220665fc34d5ffbf4ea1f1fa249409dd79d8
xvap <tahaskali@gmail.com>
2020-10-15 13:57:12 +0200

    update

---
+++ anonclient/Helper.cs
@@ -0,0 +1,34 @@
+﻿namespace AnonFile_Uploader
+{
+    class Helper
+    {
+        public static string ApiKey()
+        {
+            string url = "https://api.anonfile.com/upload?token=yourtoken";
+            return url;
+        }
+
+        public static string Pars(string strSource, string strStart, string strEnd, int startPos = 0, string error = null)
+        {
+            string result;
+            try
+            {
+                int length = strStart.Length;
+                string text = "";
+                int num = strSource.IndexOf(strStart, startPos);
+                int num2 = strSource.IndexOf(strEnd, num + length);
+                bool flag = num != -1 & num2 != -1;
+                if (flag)
+                {
+                    text = strSource.Substring(num + length, num2 - (num + length));
+                }
+                result = text;
+            }
+            catch
+            {
+                result = error;
+            }
+            return result;
+        }
+    }
+}
